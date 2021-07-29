80d7800a38f8c8f800fa3ac2e4510dc45b153184
TheNoob27 <bwsuperweevil@gmail.com>
2021-07-29 04:27:50 +0100

    fetch members command cause why not

---
+++ src/commands/fetch-members.ts
@@ -0,0 +1,7 @@
+import { Command } from "../classes/CommandManager";
+
+export default (async function code(message, args, { respond }) {
+  if (!message.guild) return
+  await message.guild.members.fetch()
+  return respond(`Successfully fetched ${message.guild.members.cache.size}/${message.guild.memberCount} members!`)
+}) as Command["run"]
